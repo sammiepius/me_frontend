261efd77bb216a1a80839a5aac2510f4778a1ec9
sammiepius <sammiepius10@gmail.com>
2025-09-09 23:22:32 +0100

    changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,91 @@
 import React from 'react';
-import { IoIosNotificationsOutline,IoIosSettings  } from 'react-icons/io';
-
+import { IoIosNotificationsOutline, IoIosSettings } from 'react-icons/io';
 
 export default function Profile() {
   return (
     <div>
       <div className="min-h-80 dark:bg-slate-800 gap-6 flex items-center justify-center">
-        <div className="absolute top-0 right-0 size-16">
-          {/* <IoIosNotificationsOutline
-            className="absolute top-0 right-0 size-8"
-            style={{ color: 'white' }}
-          /> */}
-          <IoIosSettings className="absolute top-0 right-0 size-6" style={{ color: 'white' }}/>
+        <div className="">
+          <button
+            type="button"
+            className="absolute rounded-full top-0 right-9 p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+            <span className="absolute -inset-1.5"></span>
+            <span className="sr-only">View notifications</span>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              data-slot="icon"
+              aria-hidden="true"
+              className="size-6">
+              <path
+                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+          <button
+            type="button"
+            className="absolute rounded-full top-0 right-0 p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+            <span className="absolute -inset-1.5"></span>
+            <span className="sr-only">View notifications</span>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1.5"
+              data-slot="icon"
+              aria-hidden="true"
+              className="size-6">
+              <path
+                d="M9.954 2.21a10 10 0 0 1 4.09-.002A4 4 0 0 0 16 5.07a4 4 0 0 0 3.457.261A10 10 0 0 1 21.5 8.877a4 4 0 0 0-1.5 3.122c0 1.264.586 2.391 1.501 3.124a10 10 0 0 1-2.045 3.543a4 4 0 0 0-3.456.261a4 4 0 0 0-1.955 2.86a10 10 0 0 1-4.09.004A4 4 0 0 0 8 18.927a4 4 0 0 0-3.457-.26A10 10 0 0 1 2.5 15.121A4 4 0 0 0 4 11.999c0-1.264-.587-2.39-1.502-3.124a10 10 0 0 1 2.045-3.542A4 4 0 0 0 8 5.07a4 4 0 0 0 1.954-2.86M12 15a3 3 0 1 0 0-6a3 3 0 0 0 0 6"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+          {/* <button className="absolute flex rounded-full top-0 right-0 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+              <span className="absolute -inset-1.5"></span>
+              <span className="sr-only">Open user menu</span>
+              <img
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+                className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
+              />
+            </button> */}
+
+          {/* <el-menu
+              anchor="bottom end"
+              popover
+              className="w-48 origin-top-right rounded-md bg-gray-800 py-1 outline -outline-offset-1 outline-white/10 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">
+                Your profile
+              </a>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">
+                Settings
+              </a>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">
+                Sign out
+              </a> */}
         </div>
+        {/* <div className="">
+          <IoIosNotificationsOutline
+            className="absolute top-0 right-5 size-8"
+            style={{ color: 'white' }}
+          />
+          <IoIosSettings
+            className="absolute top-0 right-0 size-6"
+            style={{ color: 'white' }}
+          />
+        </div> */}
         <div className=" dark:bg-gray-700 relative shadow-xl overflow-hidden hover:shadow-2xl group rounded-xl p-5 ">
           <div className="flex items-center gap-4">
             <img
